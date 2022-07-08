a591ef8be230ff5ef644877d185458b86c76b182
itsabdiev <73123509+itsabdiev@users.noreply.github.com>
2022-07-09 00:20:20 +0600

    Delete week7/demo directory

--- week7/demo/src/main/java/com/example/demo/Repository/DonutShopRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-
-import com.example.demo.Entity.Donut;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DonutShopRepository  extends JpaRepository<Donut,Long> {
-
-}
--- week7/demo/src/main/resources/db/migration/V1__Create.sql
+++
@@ -1,2 +0,0 @@
-create sequence donut_sequence start 1 increment 1;
-create table donuts(id int8 not null,description TEXT not null,name TEXT not null,picture TEXT not null,price int4 not null, primary key (id));
